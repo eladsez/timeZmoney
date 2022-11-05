4c600c4da5ee9f4fd75a3abedb11c186b338194f
eladsez <eladsez36@gmail.com>
2022-11-05 12:07:45 +0200

    firebase setup

---
+++ lib/Business_Logic/User.dart
@@ -0,0 +1,16 @@
+
+class User{
+
+  var _username;
+  var _email;
+  var _phone_num;
+  var _gender;
+  var _age;
+
+  User(this._username, this._email, this._gender, this._age, [this._phone_num]);
+
+
+}
+
+
+
