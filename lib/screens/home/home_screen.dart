c26d3a88a6781aa1d79dcd34c825e4fa5ca3760c
Nerya Bigon <nerya0001@gmail.com>
2023-01-15 00:12:43 +0200

    color fix

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -19,6 +19,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: theme.backgroundColor,
       body: Stack(
         children: [
           Row(
